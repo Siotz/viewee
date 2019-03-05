18643af620387f03255bace6b1fe6a5c4eb704a3
ShimSoyoung <so@ShimSoyoungui-MacBookPro.local>
2019-03-06 04:01:24 +0900

    드라마 상세 페이지 구조 설정

---
+++ src/components/DetailView.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import DramaBackground from "../containers/DramaBackground";
+
+export default class DetailView extends Component {
+  render() {
+    return (
+      <>
+        <DramaBackground />
+      </>
+    );
+  }
+}
