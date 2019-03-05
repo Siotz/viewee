6bd50a0665e8bd63d00eb8e990c5dc58a568f925
ShimSoyoung <so@ShimSoyoungui-MacBookPro.local>
2019-03-06 04:10:28 +0900

    드라마 상세페이지 드라마 정보 부분 구성

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import DramaBackground from "../containers/DramaBackground";
+import DramaInformation from "../containers/DramaInformation";
 
 export default class DetailView extends Component {
   render() {
     return (
       <>
         <DramaBackground />
+        <DramaInformation />
       </>
     );
   }
---
+++ src/containers/DramaInformation.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import s from "../scss/DramaInformation.module.scss";
+
+export default class DramaInformation extends Component {
+  render() {
+    return <div>fhk</div>;
+  }
+}
