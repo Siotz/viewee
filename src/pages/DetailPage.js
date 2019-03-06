b5bbf2b00d9fd4ffc831d5c301c33382acddab40
ShimSoyoung <so@ShimSoyoungui-MacBookPro.local>
2019-03-06 21:03:01 +0900

    match.params prop내려줌

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react'
-import Layout from '../components/Layout';
-import Detail from '../containers/Detail';
+import React, { Component } from "react";
+import Layout from "../components/Layout";
+import Detail from "../containers/Detail";
 
 export default class DetailPage extends Component {
   render() {
+    const { match } = this.props;
+    const dramaId = match.params.dramaId;
+    console.log(dramaId);
     return (
       <Layout>
-          <Detail />
+        <Detail dramaId={dramaId} />
       </Layout>
-    )
+    );
   }
 }
