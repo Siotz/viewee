5ed09fb8e125f0bd663b43636d6dbee870110475
westbro <westbro00@gmail.com>
2019-03-06 17:48:13 +0900

    Modify EditorTemplate

--- src/components/EditorTemplate.js
+++ src/components/EditorTemplate.js
@@ -1,38 +1,59 @@
 import React, { Component } from 'react'
+import { Editor, EditorState, RichUtils } from 'draft-js';
+import { stateToHTML } from "draft-js-export-html";
 import s from '../scss/EditorTemplate.module.scss';
 
 export default class EditorTemplate extends Component {
     constructor(props) {
         super(props)
-
         this.state = {
-            html: ''
+            editorState: EditorState.createEmpty(),
+            edit: false
         }
     }
 
-    handleChange(e) {
-        console.log("onChange");
+    onChange = (editorState) => {
+        this.setState({
+            editorState,
+            editorContentHtml: stateToHTML(editorState.getCurrentContent())
+        })
     }
 
-    handleSave(e) {
-        console.log(e.previousElementSibling);
-        // this.setState({ html: e.target.value });
+    onSave = (e) => {
+        // console.log(stateToHTML(this.state.editorState.getCurrentContent()));
+        console.log(this.state.editorContentHtml);
     }
+
+    onUnderlineClick = () => {
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+    }
+
+    onBoldClick = () => {
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'))
+    }
+
+    onItalicClick = () => {
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'))
+    }
+
     render() {
         return (
             <div>
                 Editor Template
-                <div className={s.content}
-                    // html={this.state.html}
-                    value={this.state.html}
-                    contentEditable={true}
-                    // name="editor"
-                    // id="editor"
-                    // suppressContentEditableWarning={true}
-                    onChange={e => this.handleChange(e)}>
-                    <p>Write your viewee.</p>
+                <button onClick={this.onUnderlineClick}>U</button>
+                <button onClick={this.onBoldClick}><b>B</b></button>
+                <button onClick={this.onItalicClick}><em>I</em></button>
+                <div className={s.content}>
+                    <Editor
+                        editorState={this.state.editorState}
+                        onChange={this.onChange}
+                        readOnly={this.state.edit}
+                    />
                 </div>
-                <button onClick={this.handleSave}>저장</button>
+                <div>
+                    <pre>{this.state.editorContentHtml}</pre>
+                </div>
+                <button onClick={this.onSave}>저장</button>
             </div>
         )
     }
