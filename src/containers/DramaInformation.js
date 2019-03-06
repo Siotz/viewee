9fe75b8eaf319a406189ba93d2ce899cc6010dab
ShimSoyoung <so@ShimSoyoungui-MacBookPro.local>
2019-03-06 21:03:48 +0900

    레이아웃 설계

--- src/containers/DramaInformation.js
+++ src/containers/DramaInformation.js
@@ -10,12 +10,13 @@ export default class DramaInformation extends Component {
       title: "워킹 데드",
       title_eng: "Walking Dead",
       season: 1,
-      year: "2019.01.01",
-      age: "19",
+      year: "2019.01",
+      age: "청불",
       country: "미국",
       tags: ["좀비", "아포칼립스", "드라마", "생존", "리더"],
       poster:
-        "https://image.tmdb.org/t/p/w440_and_h660_face/48SObcY3kYvZLFzdqiSXuzNtBMu.jpg"
+        "https://image.tmdb.org/t/p/w440_and_h660_face/48SObcY3kYvZLFzdqiSXuzNtBMu.jpg",
+      rating: 4.2
     };
   }
 
@@ -28,16 +29,29 @@ export default class DramaInformation extends Component {
       age,
       country,
       tags,
-      poster
+      poster,
+      rating
     } = this.state;
     return (
       <section className={s.DramaInformation}>
         <div className={s.poster}>
           <img src={poster} alt={title} />
         </div>
-        <h1 className={s.title}>
+        <h3 className={s.title}>
           {title} 시즌 {season}
-        </h1>
+          <p className={s.title_eng}>{title_eng}</p>
+        </h3>
+        <span className={s.rating}>★ {rating}</span>
+        <ul className={s.tags}>
+          {tags.map((t, i) => (
+            <li key={i}>{t}</li>
+          ))}
+        </ul>
+        <ul className={s.sub}>
+          <li>{year}</li>
+          <li>{age}</li>
+          <li>{country}</li>
+        </ul>
       </section>
     );
   }
