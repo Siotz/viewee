b68d38ac508b7d10bd68c5f5f83ff0668af336b6
westbro <westbro00@gmail.com>
2019-03-07 20:06:52 +0900

    solve merge conflict

--- src/pages/ReviewEditorPage.js
+++ src/pages/ReviewEditorPage.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react'
 import ReviewEditor from '../containers/ReviewEditor';
 
 export default class ReviewEditorPage extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            // true 면, /review/posting 내 글 수정하기
+            // false 면, /review/posting 새글 쓰기
+            modify: true
+        }
+    }
+
     render() {
         return (
-            <ReviewEditor modify={this.props.modify} />
+            <ReviewEditor modify={this.state.modify} />
         )
     }
 }
