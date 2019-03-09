f2f9e6eb17bf5923988ae038bcf921ab517b4eab
westbro <westbro00@gmail.com>
2019-03-09 15:34:55 +0900

    MainPage 정리

--- src/containers/ReviewDetail.js
+++ src/containers/ReviewDetail.js
@@ -37,14 +37,10 @@ class ReviewDetail extends Component {
     }
 
     async getSavedEditorData() {
-<<<<<<< HEAD
         const { reviewId } = this.props;
         const {
             data: { id, dramaId, userId, title, date, content, tags, thumbnail }
         } = await api.get(`reviews/${reviewId}`);
-=======
-        const { data: { id, dramaId, userId, title, date, content, tags, thumbnail } } = await api.get('reviews/3');
->>>>>>> 742cd56a7e6fc307f60f7059510ab5f43b7eddac
         this.setState({
             id,
             dramaId,
