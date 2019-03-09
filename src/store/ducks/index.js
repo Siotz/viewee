3b96745d13b1214564d0d2afb583efbbdd9eb51e
westbro <westbro00@gmail.com>
2019-03-09 17:19:19 +0900

    solve merge conflict

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -1,21 +1,11 @@
 import { combineReducers } from "redux";
 import page from "./page";
-<<<<<<< HEAD
 import editorState from './editorState';
+import dramas from './dramas';
 // import 기능별모듈.js파일 from '파일경로';
 
 export default combineReducers({
     page,
-    editorState
+    editorState,
+    dramas
 });
-=======
-import dramas from "./dramas";
-import editorState from "./editorState";
-// import 기능별모듈.js파일 from '파일경로';
-
-export default combineReducers({
-  page,
-  editorState,
-  dramas
-});
->>>>>>> 3c2d9022df106393ede8a6b2ba7d762f2c648346
